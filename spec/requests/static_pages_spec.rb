2871a2bdc2738b1eea50a3a39a1157b2df9217d1
Brandon Gannicott <brandon.gannicott@gmail.com>
2013-09-05 18:50:21 -0400

    Finished static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,16 +3,41 @@ require 'spec_helper'
 describe "StaticPages" do
 
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    it "should have the content 'Task2 Website'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Task2 Website')
+    end
+
+    it "should have the title 'Home'"  do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                                :text => 'task2_website | Home')
     end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
+    end
+    it "should have the title 'Help'"  do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                                :text => 'task2_website | Help')
     end
   end
+
+  describe "About page" do
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About')
+    end
+    it "should have the title 'About'"  do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                                :text => 'task2_website | About')
+    end
+  end
+
+
 end
